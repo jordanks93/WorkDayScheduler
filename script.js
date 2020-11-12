2740e845f29719fb8d8254f4f66e254aab75b27d
Jordanks93 <69221166+jordanks93@users.noreply.github.com>
2020-11-11 21:29:34 -0500

    added colorUpdate method, updated currentDay text

--- script.js
+++ script.js
@@ -1,10 +1,45 @@
-//testing
+const t = moment();
+
 var now = moment().hours();
 console.log(now);
-
-const t = moment();
 
 var time = t.hours();
 
 console.log(time);
+console.log(t.format("MMMM, Do YYYY"));
+console.log(t.format("hA"));
 
+console.log($("textarea").get(0).id);
+
+
+
+console.log(workHours);
+////////////////
+//testing above
+var workHours = $("textarea").get();
+console.log(workHours.length);
+
+
+
+
+// updates the color of the textarea element depending on the time
+function colorUpdate() {
+    var currentHour = t.hours();
+    for(i=0; i< workHours.length; i++) {
+       var hour = $("textarea").get(i).id;
+        if(currentHour > hour) {
+           $("#"+hour).addClass("past");
+        }
+        else if (currentHour === hour) {
+            $("#"+hour).addClass("present");
+         }
+        else {
+            $("#"+hour).addClass("future");
+        }
+    } 
+}
+
+// adds current date at the top of the page in currentDay element
+$("#currentDay").text(t.format("dddd, MMMM Do"));
+
+colorUpdate();
