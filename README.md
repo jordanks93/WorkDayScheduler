6a9ff25642993063588a904680d103c30cf7c8ac
Jordanks93 <69221166+jordanks93@users.noreply.github.com>
2020-11-14 09:15:21 -0500

    updated README and commented code files

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # WorkDayScheduler
 ## A simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+#List of JS methods and listeners
+
+* colorUpdate()
+  * updates the color of the textarea element depending on the time
+
+* pullActivities()
+  * gets text saved in textareas if available in local storage
+
+* (".saveBtn").click
+  *  save button listener, will save text in textarea if button clicked
+
+## Visual:
+![Start Page](./gif\Demo.gif)
+
+[Gitpages Link](https://jordanks93.github.io/WorkDayScheduler/)
+
+## Author: 
+Jordan Stuckman
--- script.js
+++ script.js
@@ -1,32 +1,36 @@
+// construtor so moment can be called more easily
 const t = moment();
+// array used to get the number of textareas 
 var workHours = $("textarea").get();
 
 // updates the color of the textarea element depending on the time
 function colorUpdate() {
     var currentHour = t.hours();
-    for(i=0; i< workHours.length; i++) {
-       var hour = $("textarea").get(i).id;
-        if(currentHour > hour) {
-           $("#"+hour).addClass("past");
+    for (i = 0; i < workHours.length; i++) {
+        var hour = $("textarea").get(i).id;
+        if (currentHour > hour) {
+            $("#" + hour).addClass("past");
         }
         else if (currentHour == hour) {
-            $("#"+hour).addClass("present");
-         }
-        else {
-            $("#"+hour).addClass("future");
+            $("#" + hour).addClass("present");
         }
-    } 
-}
-
-function pullActivities() {
-    for (i=9; i< workHours.length+9; i++) {
-        var text = localStorage.getItem(i);
-        $("#"+i).val("");
-        $("#"+i).val(text);
+        else {
+            $("#" + hour).addClass("future");
+        }
     }
 }
 
-$(".saveBtn").click(function() {
+// gets text saved in textareas if available in local storage
+function pullActivities() {
+    for (i = 9; i < workHours.length + 9; i++) {
+        var text = localStorage.getItem(i);
+        $("#" + i).val("");
+        $("#" + i).val(text);
+    }
+}
+
+// save button listener, will save text in textarea if button clicked
+$(".saveBtn").click(function () {
     var activity = $(this).siblings("textarea").val();
     var hour = $(this).siblings("textarea").get(0).id;
     localStorage.setItem(hour, activity);
@@ -37,5 +41,6 @@ $(".saveBtn").click(function() {
 // adds current date at the top of the page in currentDay element
 $("#currentDay").text(t.format("dddd, MMMM Do"));
 
+// calls methods to run when page loads
 colorUpdate();
 pullActivities();
